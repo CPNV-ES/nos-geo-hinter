a656139769cd196d4c4b24a81e1f9f9613264441
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2023-12-07 10:54:15 +0100

    add country model

---
+++ app/Models/Country.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'countries';
+}
