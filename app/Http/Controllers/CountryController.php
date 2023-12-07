650d6df19bdf7c05369f55f7ca069d989991152d
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2023-12-07 10:56:31 +0100

    edit to render as json pending on views

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -12,9 +12,7 @@ class CountryController extends Controller
      */
     public function index()
     {
-        return view('countries.index', [
-            'countries' => Country::all()
-        ]);
+        return Country::all();
     }
 
     /**
