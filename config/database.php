e5cd865a6eeac6adf1cc5670f6f028c5ed307dc6
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-12-07 09:05:24 +0100

    add the connection config in database

--- config/database.php
+++ config/database.php
@@ -34,7 +34,17 @@ return [
     */
 
     'connections' => [
-
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'options' => [
+                'database' => 'admin' // sets the authentication database required by mongo 3
+            ]
+        ],
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
