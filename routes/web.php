e0869c87285972ae728ed20e4cadc3dd8d245b54
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-01-10 09:11:51 +0100

    add component test view

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test/component', function () {
+    return view('components/test');
+});
