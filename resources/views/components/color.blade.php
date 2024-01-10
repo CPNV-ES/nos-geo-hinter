6f9f8618b9a707978d375bb16efd78ff66a5b089
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-01-10 10:15:12 +0100

    add structure

--- resources/views/components/color.blade.php
+++ resources/views/components/color.blade.php
@@ -1,3 +1,4 @@
-<div>
-
+<div class="flex gap-2 items-center bg-white rounded border border-gray-200">
+    <span class="h-10 block w-10 rounded" style="background-color: #{{ $color->hex }}"></span>
+    <div class="mr-3">{{ $color->name }}</div>
 </div>
