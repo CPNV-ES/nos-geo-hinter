c8b6575fd21de249d3eb559e54d1535becbe00b5
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-01-11 08:10:39 +0100

    Add country route

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("/country/{country}", function (Country $country) {
+    return $country;
+})->name("country.show");
