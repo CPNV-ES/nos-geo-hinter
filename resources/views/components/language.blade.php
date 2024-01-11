210f54f5b60438ad4a44bca0daa12b65448a97db
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-01-11 09:36:55 +0100

    add jetbrains mono as default tailwind monospaced font

--- resources/views/components/language.blade.php
+++ resources/views/components/language.blade.php
@@ -2,9 +2,9 @@
     <h6 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $language->name }}</h6>
     <ul>
         @foreach($language->characters as $character)
-            <li class="border border-dashed border-gray-50 dark:border-gray-700 aspect-square h-fit flex flex-col justify-center dark:text-white text-center p-2">
-                <p class="jb-mono block text-2xl">{{ $character->char }}</p>
-                <p class="jb-mono text-sm text-gray-500">{{ $character->unicode }}</p>
+            <li class="border border-dashed border-gray-100 dark:border-gray-700 aspect-square w-20 flex flex-col justify-center dark:text-white text-center p-2">
+                <p class="mono block text-2xl">{{ $character->char }}</p>
+                <p class="mono text-sm text-gray-500">{{ $character->unicode }}</p>
             </li>
         @endforeach
     </ul>
