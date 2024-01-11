54db8a63124f2d9ab7ce484371e27bf1cbe74076
vicmtn <victorien.montavon@gmail.com>
2024-01-11 09:13:52 +0100

    create layout for flags view, lack import of each components

---
+++ resources/views/flags.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="main-container">
+    <div class="pt-20">
+        @component('components.navbar')
+        @endcomponent
+    </div>
+    <div class="text-4xl">Europe</div>
+    <br>
+    <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <!-- etcc... -->
+    </div>
+    <div class="text-4xl pt-6">Asie</div>
+    <br>
+    <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <!-- etcc... -->
+    </div>
+    <div class="text-4xl pt-6">Amérique du Sud</div>
+    <br>
+    <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <!-- etcc... -->
+    </div>
+    <div class="text-4xl pt-6">Amérique du Nord</div>
+    <br>
+    <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <!-- etcc... -->
+    </div>
+    <div class="text-4xl pt-6">Afrique</div>
+    <br>
+    <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <!-- etcc... -->
+    </div>
+    <div class="text-4xl pt-6">Océanie</div>
+    <br>
+    <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <!-- etcc... -->
+    </div>
+</div>
+</body>
+</html>
