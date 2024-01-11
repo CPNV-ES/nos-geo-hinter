0c98d9191f37d9ad99a8ee8445c5c48b8d846386
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-01-11 08:59:01 +0100

    create component chars

--- resources/views/components/language.blade.php
+++ resources/views/components/language.blade.php
@@ -1,3 +1,11 @@
 <div>
-
+    <h6 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $language->name }}</h6>
+    <ul>
+        @foreach($language->characters as $character)
+            <li class="border border-dashed border-gray-50 dark:border-gray-700 aspect-square h-fit flex flex-col justify-center dark:text-white text-center p-2">
+                <p class="jb-mono block text-2xl">{{ $character->char }}</p>
+                <p class="jb-mono text-sm text-gray-500">{{ $character->unicode }}</p>
+            </li>
+        @endforeach
+    </ul>
 </div>
