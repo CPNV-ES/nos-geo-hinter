3c28a7916252f50bc005682169c5b476da4d23a1
vicmtn <victorien.montavon@gmail.com>
2024-01-11 09:16:39 +0100

    create layout for index view lack import needed components and link to data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,30 @@
-@component('layouts.components.navbar')
-@endcomponent
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>GeoHintr</title>
+</head>
+<body>
+<div class="main-container">
+    <div class="pt-20">
+        @component('components.navbar')
+        @endcomponent
+    </div>
+    @component('components.statistics')
+    @endcomponent
+    <div class="px-6 pt-3">
+        <div class="grid grid-cols-2 gap-6">
+            <div class="row-span-1">@component('components.top-card') @endcomponent</div>
+            <div class="col-span-1 right-0">@component('components.top-card') @endcomponent</div>
+            <div class="row-span-2">@component('components.top-card') @endcomponent</div>
+            <div class="row-span-2">@component('components.top-card') @endcomponent</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
