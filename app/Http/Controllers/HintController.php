bc6a1fa3481f202d218ea68429d296851ac746ed
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-01-14 16:35:41 +0100

    Add Hint stats

--- app/Http/Controllers/HintController.php
+++ app/Http/Controllers/HintController.php
@@ -16,4 +16,17 @@ class HintController extends Controller
         $log->save();
         return $log;
     }
+    //count each log by scope and return the count
+    public function stats()
+    {
+        $logs = Log::all();
+        $stats = [];
+        foreach ($logs as $log) {
+            if (!isset($stats[$log->scope])) {
+                $stats[$log->scope] = 0;
+            }
+            $stats[$log->scope]++;
+        }
+        return $stats;
+    }
 }
