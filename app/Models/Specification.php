193c4a961d30b1c4eecd0c3e29b347b7afc2916f
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-01-14 20:42:58 +0100

    create plate component

---
+++ app/Models/Specification.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class Specification extends Model
+{
+
+}
---
+++ resources/views/test/components/plate.blade.php
@@ -0,0 +1,10 @@
+@extends('test.components.layout')
+
+@section('content')
+    @foreach($countries as $country)
+        <div class="p-6 m-6 border-2 border-dashed border-violet-600 rounded-lg place-self-center dark:bg-gray-900 relative">
+            <div class="absolute -top-7 left-0 text-violet-600 font-semibold">{{ $country->name }}</div>
+            <x-hint.plate :plate="$country->plate"/>
+        </div>
+    @endforeach
+@endsection
