0de4739bfbbe341104fe0ffe7c6ef2356f29ebc8
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-01-15 00:38:53 +0100

    make image clicable

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
 
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <img rel="geohintr-icon" style="height: 6dvh" src="/icon.svg">
+        <a href="/"><img rel="geohintr-icon" style="height: 6dvh" src="/icon.svg"></a>
         <x-search-bar/>
         <div class="flex">
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse pl-6">
