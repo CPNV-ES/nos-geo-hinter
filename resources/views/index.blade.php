c4b1a23808d6b8b29da8992ac7f2a3414396d5c7
vicmtn <victorien.montavon@gmail.com>
2024-01-14 19:07:37 +0100

    change the calling method for components

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,10 +18,10 @@
     @endcomponent
     <div class="px-6 pt-3">
         <div class="grid grid-cols-2 gap-6">
-            <div class="row-span-1">@component('components.top-card') @endcomponent</div>
-            <div class="col-span-1 right-0">@component('components.top-card') @endcomponent</div>
-            <div class="row-span-2">@component('components.top-card') @endcomponent</div>
-            <div class="row-span-2">@component('components.top-card') @endcomponent</div>
+            <div class="row-span-1"><x-top-card></x-top-card></div>
+            <div class="col-span-1 right-0"><x-top-card></x-top-card></div>
+            <div class="row-span-2"><x-top-card></x-top-card></div>
+            <div class="row-span-2"><x-top-card></x-top-card></div>
         </div>
     </div>
 </div>
