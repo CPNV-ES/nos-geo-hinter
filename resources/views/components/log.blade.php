9d3e1c2b2afd90d3968b9b708b1b1775bba36b56
vicmtn <victorien.montavon@gmail.com>
2024-01-14 22:38:09 +0100

    add style to log component and call to right value

--- resources/views/components/log.blade.php
+++ resources/views/components/log.blade.php
@@ -1,10 +1,10 @@
-<div>
-    <form action="{{ route('hint.log') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <input type="hidden" name="country_id" value="{{$country->}}">
-            <input type="hidden" name="scope" value="{{$scope}}">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+<div class="pt-4">
+        <form action="{{ route('hint.log') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <input type="hidden" name="country_id" value="{{$country->name}}">
+                <input type="hidden" name="scope" value="{{$scope}}">
+            </div>
+            <button type="submit" class=" text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">J'aime</button>
+        </form>
+    </div>
