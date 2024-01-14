7800874ac9f8ca471496e082cc24e0eec891b80a
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-01-15 00:05:49 +0100

    update name for x-log in show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,71 +1,83 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>GeoHintr</title>
 </head>
-<body>
-<div class="pt-20">
-    @component('components.navbar')
-    @endcomponent
-</div>
-<div class="main-container pt-20">
-    <div class="container">
-        <div class="grid grid-cols-3 gap-1">
-            <div class=""><h1 class="text-4xl underline">Nom du pays</h1></div>
-            <div class=""><img
-                    src="https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg"
-                    style="height: 50px; width: 50px" alt="Drapeau du pays"></div>
-            <div class=""><img src="{{$country->flag->image}}" style="height: 50px; width: 50px"
-                               alt="Logo Street View"></div>
-        </div>
-    </div>
-    <br>
-    <div class="grid grid-cols-2">
-        <div class="grid grid-row-3 gap-1">
-            <div class=""><h1 class="text-3xl">Drapeau</h1></div>
-            <div class=""><img class="text-2xl" src="#" alt="Layout du drapeau"></div>
-            <div class=""></div>
-            <x-log :country="$country" scope='Forme'></x-log>
-        </div>
-        <div class="grid grid-rows-2 gap-1">
-            <div class=""><h1 class="text-2xl">Spécial Char</h1>
-                <div class="text-lg">ñ</div>
-                <div>
-                    <x-log :country="$country" scope='Charactère'></x-log>
-                </div>
 
-                <div class=""><h1 class="text-2xl">Language</h1>
-                    <div class="text-lg">Anglais</div>
-                    <div>
-                        <x-log :country="$country" scope='Langue'></x-log>
-                    </div>
+<body>
+    <div class="pt-20">
+        @component('components.navbar')
+        @endcomponent
+    </div>
+    <div class="main-container pt-20">
+        <div class="container">
+            <div class="grid grid-cols-3 gap-1">
+                <div class="">
+                    <h1 class="text-4xl underline">Nom du pays</h1>
                 </div>
-                <div class="grid grid-row-3 gap-1">
-                    <div class=""><h1 class="text-3xl">Panneaux</h1></div>
-                    <div class="">Autoroute :</div>
-                    <div class="">Route principales :
-                        <x-color></x-color>
-                    </div>
+                <div class=""><img
+                        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg"
+                        style="height: 50px; width: 50px" alt="Drapeau du pays"></div>
+                <div class=""><img src="{{ $country->flag->image }}" style="height: 50px; width: 50px"
+                        alt="Logo Street View"></div>
+            </div>
+        </div>
+        <br>
+        <div class="grid grid-cols-2">
+            <div class="grid grid-row-3 gap-1">
+                <div class="">
+                    <h1 class="text-3xl">Drapeau</h1>
+                </div>
+                <div class=""><img class="text-2xl" src="#" alt="Layout du drapeau"></div>
+                <div class=""></div>
+                <x-log :country="$country" scope='Forme'></x-log>
+            </div>
+            <div class="grid grid-rows-2 gap-1">
+                <div class="">
+                    <h1 class="text-2xl">Spécial Char</h1>
+                    <div class="text-lg">ñ</div>
                     <div>
-                        <x-log :country="$country" scope="Panneaux"></x-log>
+                        <x-log :country="$country" scope='Caractère'></x-log>
                     </div>
-                    <div class="grid grid-row-3 gap-1">
-                        <div class=""><h1 class="text-3xl">Drapeau</h1></div>
-                        <div class=""><img class="text-2xl" src="#" alt="Layout du drapeau"></div>
-                        <div class=""></div>
+
+                    <div class="">
+                        <h1 class="text-2xl">Language</h1>
+                        <div class="text-lg">Anglais</div>
                         <div>
-                            <x-log :country="$country" scope="Drapeau"></x-log>
+                            <x-log :country="$country" scope='Langue'></x-log>
                         </div>
                     </div>
+                    <div class="grid grid-row-3 gap-1">
+                        <div class="">
+                            <h1 class="text-3xl">Panneaux</h1>
+                        </div>
+                        <div class="">Autoroute :</div>
+                        <div class="">Route principales :
+                            <x-color></x-color>
+                        </div>
+                        <div>
+                            <x-log :country="$country" scope="Panneau"></x-log>
+                        </div>
+                        <div class="grid grid-row-3 gap-1">
+                            <div class="">
+                                <h1 class="text-3xl">Drapeau</h1>
+                            </div>
+                            <div class=""><img class="text-2xl" src="#" alt="Layout du drapeau"></div>
+                            <div class=""></div>
+                            <div>
+                                <x-log :country="$country" scope="Drapeau"></x-log>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
-            </div>
-
 </body>
-</html>
 
+</html>
