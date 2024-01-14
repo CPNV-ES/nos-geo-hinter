7fa1794ac269a3472f0184cce6db9d12f1bc4b7a
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-01-15 00:37:22 +0100

    update button on flag component

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,7 +12,7 @@
 @component('components.navbar')
 @endcomponent
 <div class="max-w-screen-xl mx-auto px-3">
-    <div class="border-b border-b-gray-400 pb-2 flex gap-2 mb-5">
+    <div class="border-b border-b-gray-400 pb-2 flex gap-4 mb-5">
         <img src="{{ $country->flag->image }}" alt="Drapeau {{ $country->name }}" class="h-16 rounded">
         <div>
             <h1 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $country->name }}</h1>
@@ -20,10 +20,7 @@
         </div>
     </div>
     <div class="grid gap-4 grid-cols-2">
-        <div class="">
-            <x-hint.flag :flag="$country->flag"/>
-            <x-log :country="$country" scope='Forme' class="absolute top-3 right-3"></x-log>
-        </div>
+        <x-hint.flag :$country/>
         <div class="">
             <div class="block max-w-2xl p-4 min-w-max bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:text-gray-50 text-gray-900">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight">Langues</h5>
